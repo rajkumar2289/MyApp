cb94a9cdef4ead35593e278c9004b59245e559a1
rajkumar2289 <82703419+rajkumar2289@users.noreply.github.com>
2021-05-14 11:41:35 +0530

    Update App.java

--- main/java/com/maaven/practice/App.java
+++ main/java/com/maaven/practice/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Add Webhook Test!");
+        System.out.println("Add  Test!");
     }
 }
